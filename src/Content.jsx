0c5c4358da34d3266abc2507f833644e7b6574e4
Juwon Do <nobeldr13@gmail.com>
2023-01-11 13:31:14 -0600

    add Modal

--- src/Content.jsx
+++ src/Content.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from "axios";
 import { PostNew } from './PostNew';
 import { PostIndex } from './PostIndex';
+import { Modal } from "./Modal";
 
 export function Content() {
   const [posts, setPosts] = useState([]);
@@ -20,6 +21,9 @@ export function Content() {
       <PostNew />
       {/* <button onClick={handleIndexPosts}>Get the data</button> */}
       <PostIndex posts={posts} />
+      <Modal show={true}>
+        <p>TEST</p>
+      </Modal>
     </div>
   );
 }
